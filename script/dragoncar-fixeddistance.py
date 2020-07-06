a990224b7d2642ad9b5cc256ffc5657d5f45eda5
bibo <bibo19842003@163.com>
2020-07-07 05:34:21 +0800

    optimize fixed distance

--- script/dragoncar-fixeddistance.py
+++ script/dragoncar-fixeddistance.py
@@ -9,12 +9,12 @@ chaoshengbo = DistanceSensor(27, 22)
 while True:
     if(chaoshengbo.distance > 0.3):
         print("qian")
-        robot.forward(speed=0.5)
+        robot.forward(speed=0.3)
     elif(chaoshengbo.distance < 0.2):
         print("hou")
-        robot.backward(speed=0.5)
+        robot.backward(speed=0.3)
     else:
         print("ting")
         robot.stop()
 
-    sleep(0.5)
+    sleep(0.1)
